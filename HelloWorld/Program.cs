0d2d848e8cd7bcd419bc61a70407323c4d3fe668
D'Miria Collins <collins.dmiria@gmail.com>
2017-11-07 18:35:21 -0500

    Updated Program Calculator
    Included code to allow the user to continuously perform operations until they enter "end"

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,8 +1,9 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
 
 namespace HelloWorld
 {
@@ -37,9 +38,10 @@ namespace HelloWorld
         }
 
         //Returns the absolute value of num1 - num2
-        public int AbsSubtract(int num1, int num2){
-            return num1 - num2;
-         }
+        public int AbsSubtract(int num1, int num2)
+        {
+            return Math.Abs(num1 - num2);
+        }
 
         //Returns the result of num1 raised to num2
         // Multiply num1 times num1, num2 times
@@ -52,7 +54,7 @@ namespace HelloWorld
                 return 1;
             }
 
-            for (int i = 0; i>= num2; i += 1)
+            for (int i = 0; i >= num2; i += 1)
             {
                 result *= num1;
 
@@ -68,38 +70,80 @@ namespace HelloWorld
         static void Main(string[] args)
         {
             string a, b, c;
-            int result;
+            int result = -1;
             Operations o = new Operations();
-            
-            Console.WriteLine("Hello!");
-            Console.WriteLine("Welcome to My First C# Calculator");
-            Console.WriteLine("Enter the first value.");
-            a = Console.ReadLine();
-            Console.WriteLine("Enter the second value.");
-            b = Console.ReadLine();
-            Console.WriteLine("Enter the operator you want to use.");
-            c = Console.ReadLine();
+            bool calc = true;
 
-            if (c == "+")
+            while (calc == true)
             {
-               result = o.AddVal(Convert.ToInt32(a),Convert.ToInt32(b));
-            } else if (c == "-"){
-                result = o.SubtractVal(Convert.ToInt32(a), Convert.ToInt32(b));
-            }else if (c == "*")
-            {
-                result = o.MultiplyVal(Convert.ToInt32(a), Convert.ToInt32(b));
-            }else if (c == "/")
-            {
-                result = o.DivideVal(Convert.ToInt32(a), Convert.ToInt32(b));
-            }else if (c == "^")
-            {
-                result = o.PowerVal(Convert.ToInt32(a), Convert.ToInt32(b));
-            }else
-            {
-                result = 0;
+                Console.WriteLine("Hello!");
+                Console.WriteLine("Welcome to My First C# Calculator");
+                Console.WriteLine("Enter the first value.");
+                a = Console.ReadLine();
+                if (a == "end")
+                {
+                    //calc = false;
+                    break;
+                }
+                Console.WriteLine("Enter the operator you want to use.");
+                c = Console.ReadLine();
+                if (c == "end")
+                {
+                    //calc = false;
+                    break;
+                }
+                Console.WriteLine("Enter the second value.");
+                b = Console.ReadLine();
+                if (b == "end")
+                {
+                    //calc = false;
+                    break;
+                }
+                /*The current issue is that the end string doesn't kill the calc because I'm reading all the lines and then 
+                 * performing the check through the if statements. That happens after the user is prompted. 
+                 * 
+                 * 
+                 * Solution: check to see if the user enters end at each level
+                 */
+
+                if (c == "+")
+                {
+                    result = o.AddVal(Convert.ToInt32(a), Convert.ToInt32(b));
+                }
+                else if (c == "-")
+                {
+                    result = o.SubtractVal(Convert.ToInt32(a), Convert.ToInt32(b));
+                }
+                else if (c == "*")
+                {
+                    result = o.MultiplyVal(Convert.ToInt32(a), Convert.ToInt32(b));
+                }
+                else if (c == "/")
+                {
+                    result = o.DivideVal(Convert.ToInt32(a), Convert.ToInt32(b));
+                }
+                else if (c == "^")
+                {
+                    result = o.PowerVal(Convert.ToInt32(a), Convert.ToInt32(b));
+                }
+                else if (c == "abs")
+                {
+                    result = o.AbsSubtract(Convert.ToInt32(a), Convert.ToInt32(b));
+                }
+                else
+                {
+                    result = 0;
+                }
+
+                Console.WriteLine("The answer is: {0} ",result);
+                Console.WriteLine(" ");
+                Console.WriteLine("NOTE: Type end to close the application");
+                Console.WriteLine(" ");
             }
-            Console.WriteLine(result);
-            Console.ReadLine();
+
+            //result = -1;
+            
+            //Console.ReadLine();
 
         }
     }
